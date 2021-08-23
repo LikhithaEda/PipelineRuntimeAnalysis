11c594f0f94cf22ab9da201d5c8f00a4e7c6052b
Likhitha Eda <leda@redhat.com>
2021-08-23 14:25:42 -0400

    new changes

--- pipelineRuntime_report.py
+++ pipelineRuntime_report.py
@@ -7,6 +7,8 @@ import pandas as pd
 from pandas import json_normalize
 import json
 from math import ceil
+import numpy as np
+import matplotlib.pyplot as plt
 
 def getData(topic,delta):
     # startURL = "https://datagrepper.engineering.redhat.com/raw?topic=/topic/"
@@ -23,7 +25,7 @@ def getData(topic,delta):
             'page': str(page),
             'rows_per_page': str(rows_per_page)
         }
-        # url = startURL + topic + deltaURL + delta
+
         response = requests.get(url, params=params, verify="/etc/pki/ca-trust/source/anchors/2015-RH-IT-Root-CA.pem")
         if response.status_code == 200: #200 means request fulfilled
             response_json = response.json()
@@ -55,6 +57,7 @@ def getStageData(delta):
     for index in range(0,len(result['raw_messages'])):
         y.append(result['raw_messages'][index]['msg']['stage'])
     StageData_df = pd.DataFrame(y) 
+    print(StageData_df)
     return StageData_df
 
 def getFirstStageData(delta):
@@ -88,12 +91,11 @@ def getFirstStageData(delta):
 
     #combine nvr+pipeline id columns 
     FirstStageData_df['nvr+pipelineID'] = FirstStageData_df['nvr'] + FirstStageData_df['pipelineID']
-    #print("start time")
-    #print(FirstStageData_df)
+   
     #convert to human readble time
     FirstStageData_df['startTime'] = convertEpoch(FirstStageData_df['startTime'])
     #FirstStageData_df.to_csv("FirstStageData.csv", encoding='utf-8', index=False)
-    #print(FirstStageData_df)
+
     return FirstStageData_df
 
 def getFinalStageData(delta):
@@ -126,9 +128,6 @@ def getFinalStageData(delta):
 
     #combine nvr+pipeline id columns 
     FinalStageData_df['nvr+pipelineID'] = FinalStageData_df['nvr'] + FinalStageData_df['pipelineID']
-
-    #print("end time")
-    #print(FinalStageData_df)
     
     #convert to human readble time
     FinalStageData_df['endTime'] = convertEpoch(FinalStageData_df['endTime'])
@@ -139,24 +138,40 @@ def calculateRuntime(start, end):
   time_diff = (end - start).total_seconds() / 60 #return diff in mins
   return int(time_diff)
 
-def createGraph(df):
-    return None
+def addlabels(x, y):
+    """create labels for bars in bar chart"""
+
+    for i in range(len(x)):
+        plt.text(i, y[i], y[i], ha='center')
+
+def createGraph(x_col, y_col):
+    
+    fig, ax = plt.subplots()
+    x_pos = np.arange(len(x_col))  # <--
+    plt.bar(x_pos, y_col)
+    plt.xticks(x_pos, x_col)  # <--
+    # Make space for and rotate the x-axis tick labels
+    fig.autofmt_xdate()
+    ax.xaxis_date()
+    addlabels(x_col, y_col)
+    plt.xlabel("Dates")
+    plt.ylabel("Run Time in Minutes")
+    plt.title("Average Run Times")
+    plt.savefig('AvgRunTimes.png', dpi=400)
+    plt.show()
 
 def main(argv):
     """read command line for delta configuration"""
-    delta = "19000" #getting delta from run command sys.argv[1]
+    delta =  sys.argv[1] #getting delta from run command 
+    
+    #get pipeline runtimes
+    #if (sys.argv[2] == 1):
     #get the data
     FirstStage = getFirstStageData(delta)
     FinalStage = getFinalStageData(delta)
-    #stageData = getStageData(delta)
-
-    #create a main dataframe for pipeline executions
-    #runtimes_df = pd.concat([FirstStage['timestamp'], FinalStage['timestamp']], axis=1, keys=['start time', 'end time'])
-    
     #merge Firststage data and finalstage data based on nvr
     runtimes_df= pd.merge(FirstStage, FinalStage, on='nvr+pipelineID')
-    #print(runtimes_df)
-    
+        
     #calculate run time
     runtime = []
     for index, row in runtimes_df.iterrows():
@@ -167,11 +182,32 @@ def main(argv):
             runtime.append("NaN")
     #add column to df
     runtimes_df['run time(mins)'] = runtime
-        
-    #runtimes_df.to_csv("runtime.csv", encoding='utf-8', index=False)
-    #print(runtimes_df)
+            
+        #runtimes_df.to_csv("runtime.csv", encoding='utf-8', index=False)
+        #print(runtimes_df)
 
+        #create a year-month column based on runtime end date
+        #group by the year-month column and find runtime avgs
+    runtimes_df['endTime_YM'] = pd.to_datetime(runtimes_df['endTime']).dt.to_period('D')
+    new_df = runtimes_df.filter(['endTime_YM', 'run time(mins)'], axis=1)
+    group_mean = new_df.groupby('endTime_YM')['run time(mins)'].mean()
+    mean_df = group_mean.reset_index()
 
+    mean_df['run time(mins)'] = mean_df['run time(mins)'].astype(int)
+
+        #create a graph for avg pipeline runtime
+    createGraph(mean_df['endTime_YM'], mean_df['run time(mins)'])
+ 
+    #get stage runtimes
+    #elif (sys.argv[2] == 2):
+    stageData = getStageData(delta)
+    print(stageData)
+    
+    #create a main dataframe for pipeline executions
+    #runtimes_df = pd.concat([FirstStage['timestamp'], FinalStage['timestamp']], axis=1, keys=['start time', 'end time'])
+    
+    #merge Firststage data and finalstage data based on nvr
+    
 
 
 if __name__ == "__main__":
